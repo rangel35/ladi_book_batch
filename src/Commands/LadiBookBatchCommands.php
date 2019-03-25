bafc8cdea50f8ade051c8cac7c8ab9ff73e93c6a
Minnie Rangel <mrangel@austin.utexas.edu>
2019-03-25 16:30:55 -0500

    cleanup & adding multi-valued fields

--- src/Commands/LadiBookBatchCommands.php
+++ src/Commands/LadiBookBatchCommands.php
@@ -75,6 +75,9 @@ class LadiBookBatchCommands extends DrushCommands {
 		$password = 'PASSWORD';
 		$input_dir = 'UPLOAD DIRECTORY';
 
+        //Non-processing filenames or directories for use in project
+        $illNames = array(".", "..", "Thumbs.db", ".DS_Store") ;
+        
 		$this->output()->writeln('Hello from inside batchIngest.');
 
 		// select * from Gemini order by dateCreated ;
@@ -87,7 +90,7 @@ class LadiBookBatchCommands extends DrushCommands {
                 while ($row = $result->fetchAssoc()) {
                   
                     $batchID = $row['batchID'] ;
-                    $namespace = $row['namespace'] ;
+                    $nspace = $row['namespace'] ;
                     $collection = $row['collection'] ;
                     $location = $row['location'] ;
                     $userID = $row['userID'] ;
@@ -105,37 +108,52 @@ class LadiBookBatchCommands extends DrushCommands {
                     $accountSwitcher->switchTo($userSession);
                     
                     $batchrow = new BatchEntry($row, $input_dir);
-                    //echo "batchrow is" . PHP_EOL;
-                    //var_dump($batchrow);
+                    echo "batchrow is" . PHP_EOL;
+                    var_dump($batchrow);
                   
 					$msg = $batchrow->format_email_output();
-					echo "msg is: \n" . $msg . PHP_EOL;
+//					echo "msg is: \n" . $msg . PHP_EOL;
                    
-                    echo " Asset Path ===> " . $batchrow->asset_path . "\n";
+//                    echo " Asset Path ===> " . $batchrow->asset_path . "\n";
 					$dirs = scandir($batchrow->asset_path) ;					
-					var_dump($dirs);
+//					var_dump($dirs);
+                    if (empty($dirs)) {
+                        
+                        $msg .= "\r\nDir == " . $batchrow->asset_path . " is empty or does not exist. Please resubmit your batch request or contact an administrator for more information \n\r" ;
+                    }
                     
                     foreach($dirs as $dir) {
+                        //continue;
                         if ($dir=="." || $dir=="..") {continue;}
                         $this->book_path = $batchrow->asset_path . "/" . $dir ;
                         echo " Book Path ===> " . $this->book_path . "\n";
-                        $files = scandir($this->book_path);
-                        foreach($files as $f) {
-                            if ($f=="." || $f==".." || (strpos($f,'.csv') !== FALSE)) {
-                                $trash = array_shift($files) ;
-                            }else{
+                        $dirFiles = scandir($this->book_path);
+                        $files = array();  //will contain image files for use 
+                        
+                        //reads all the media files into array for later uploading
+                        foreach($dirFiles as $f) {
+                            if ($f=="." || $f==".." || (strpos($f,'.csv') !== FALSE) || (in_array($f, $illNames))) {
                                 continue;
+                            }else{
+                                $files[] = $f ;
                             }                            
                         }
                         print "files \n";
                         var_dump($files);
 
+                        if (empty($files)) {
+                        
+                            $msg .= "\r\nDir == " . $dir . "in batch " . $batchID . " is empty or only contains a csv file, so skipping processing, please resubmit in a future batch. \n\r" ;
+                            continue;
+                        }
+
+                        
                         //note dir name should match csv_file name
-                        $msg .= "working with Book " . $dir . PHP_EOL; 
+                        $msg .= "\r\n \r\n" . "working with Book " . $dir . "\r\n"; 
                         $csv_file = $this->book_path . "/" . $dir . ".csv";
                         if (!file_exists($csv_file)) {
                             echo "The file $csv_file does not exist so book $dir not processed \n";
-                            $msg .= "The file $csv_file does not exist so book $dir not processed \n";
+                            $msg .= "The file $csv_file does not exist so book $dir not processed \r\n";
                             continue;
                         }
 												
@@ -143,11 +161,20 @@ class LadiBookBatchCommands extends DrushCommands {
 
                         $fp = fopen($csv_file, 'r');
                         $headers = fgetcsv($fp, 1024, ",") ;
-        				
+                        
+                        //dups array is to hold csv repeated headers (for multivalued fields)
+                        $dups = array();
+                        foreach(array_count_values($headers) as $val => $c) {
+                            if($c > 1) $dups[] = $val;
+                        }
+        				//var_dump($dups);
+                        //continue;
+                        
                         $weight = 0;
                         $pages = array();  
                         
                         while (($import_row = fgetcsv($fp, 1024, ",")) !== FALSE) {
+                            $import_row = array_map("utf8_encode", $import_row); 
 							$num = count($import_row);
 							
 							if(preg_match('/node\/(\d+)/', $collection, $matches)) {
@@ -169,34 +196,52 @@ class LadiBookBatchCommands extends DrushCommands {
                                     'field_member_of' => $colID,   
 
                                 );                               
+                                //echo "this is the csv \n" ;
+                                //var_dump($csv_entries);
+                                //continue;
 
                                 for ($i = 1; $i <= $num; $i++) {
                                     if (!empty($headers[$i]) && !empty($import_row[$i])) {
-                                        $csv_entries[$headers[$i]] =  $import_row[$i] ;  
+                                        
+                                        if (in_array($headers[$i], $dups)) {
+                                            $csv_entries[$headers[$i]][] =  $import_row[$i] ;
+                                        } else {
+                                        	$csv_entries[$headers[$i]] =  $import_row[$i] ;  
+                                    }
+                                          
                                     }
                                 }
                                 
                                 $node = \Drupal::entityTypeManager()->getStorage('node')->create($csv_entries);
 								                                
-                                //check on alias not currently being set 
+                                //saves book node to drupal db
 								$node->save();
                                 
-							    //$node->url_alias->value = $import_row[1];     //set value for field
-                                //$node->save();
 								$bid = $node->id();                               
                                 echo "this is bid $bid \n";
                                 
-                				//$nUUID = $node->uuid() ;
-                                //echo "this is nUUID \n";
-                                //var_dump($nUUID);
+                				$nUUID = $node->uuid() ;
+                                echo "this is book UUID $nUUID \n";
+                                $bookalias = "/" . $nspace . "/" . $nUUID ;
+							    echo "this is book bookalias $bookalias \n";
+                                $system_path = "/node/" . $bid ; 
+                                $path = \Drupal::service('path.alias_storage')->save($system_path, $bookalias, "en");
+
+                                //reads next file in array for adding media
                                 $pageFile = array_shift($files) ;
                                 //call to set up book in drupal databases
                                 $this->add_book_entry_to_drupal_db($connection, $bid);
                                 $this->add_media_file($bid, $batchrow, $pageFile, $dir);
-								          
+								
+                                $url = $host . $bookalias;
+                                $msg .= "Book image ==> " . $pageFile . "\r\n"; 
+                                $msg .= "Book URL ==> " . $url . "\r\n";          
 							} else {
 
+                                //Create Page(s) for book with limited metadata and media file
+                                //continue;
                                 $weight++;
+                               
                                 echo "this is weight $weight \n";
                                 
                                 $pageFile = array_shift($files) ;
@@ -206,17 +251,28 @@ class LadiBookBatchCommands extends DrushCommands {
 								  'type'        => 'book',
 								  'title'       => $import_row[2],
 								  'uid'		=> $batchrow->userID,
-								  'field_description'       => $import_row[2],
+								  'field_identifier_local'       => $import_row[1],
 								  'field_member_of'       => $bid,
 								));
 								$node->save();
 								$pageID = $node->id();
 								$pages[] = $pageID ;
-                                
+
+                				$nUUID = $node->uuid() ;
+                                echo "this is page UUID $nUUID \n";
                                 echo "pageID is $pageID \n";
+                                $pagealias = "/" . $nspace . "/" . $nUUID ;
+                                echo "this is page pagealias $pagealias \n";
+                                $system_path = "/node/" . $pageID ; 
+                                $path = \Drupal::service('path.alias_storage')->save($system_path, $pagealias, "en");
                                 
+                                //call to add media to page & entry to drupal book db
                                 $this->add_media_file($pageID, $batchrow, $pageFile, $dir);
                                 $this->add_page_entry_to_drupal_db($connection, $pageID, $bid, $weight) ;
+                                
+                                $urlP = $host . $pagealias;
+                                $msg .= "Page image ==> " . $pageFile . "\r\n" ; 
+                                $msg .= "Page URL ==> " . $urlP . "\r\n" ; 
 
 							}
 							
@@ -224,11 +280,13 @@ class LadiBookBatchCommands extends DrushCommands {
 							
 						}
 												
-						
-					
                     }
 										
-			  }
+                    //print "email message is: \n $msg \n";
+                    $batchrow->close_batch_row($row, $input_dir);
+                    $batchrow->batch_submission_email($batchID, $userEmail,$msg);
+                }
+                
 			} else {
 				echo "no batches queued for ingest\n";
 				die;
@@ -287,14 +345,17 @@ class LadiBookBatchCommands extends DrushCommands {
         // currently hardcoded to Service File for field_media_use 
         // code to test if date directory exists if not then create
         
-        print "batchrow \n";
-        var_dump($batchrow);
+//        print "batchrow \n";
+//        var_dump($batchrow);
         $fileMedia = $batchrow->asset_path . "/" . $dir . "/" . $pageFile ;
         $pageFname = basename($fileMedia) ;
         $data = file_get_contents($fileMedia);
-        $date = date("Y-m");
         
-        $destination = 'public://' . $date . "/" . $pageFile ;
+        //TODO: add code to create new directory when month changes
+        $date = date("Y-m");
+        $publicDir = 'public://' . $date . "/" ;
+                
+        $destination = $publicDir . $pageFile ;
          print "destination is " . $destination . "\n";
 
         $fileEnt = file_save_data($data, $destination, FILE_EXISTS_RENAME);
@@ -304,15 +365,18 @@ class LadiBookBatchCommands extends DrushCommands {
         print "Target ID " . $fileEnt->id() . "\n";   
         print "fileEnt label " . $fileEnt->label() . "\n";   
 
+        //TODO: determine taxonomy id from text name (Service File)
+        //also determine bundle from mimetype/extension
+        
         $drupalMedia = Media::create([
-          'bundle' => 'file',
+          'bundle' => 'image',
           'uid' => $batchrow->userID,
           'field_media_of' => $pageID,
-          'field_media_use' => 17,
-          'field_media_file' => [
+          'field_media_use' => 18,
+          'field_media_image' => [
             'target_id' => $fileEnt->id(),
-            'alt' => t('foo'),
-            'title' => t('bar'),
+            'alt' => 'image of ' . $pageFile,
+            'title' => $pageFile,
           ],
         ]);
 
